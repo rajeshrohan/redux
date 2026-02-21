483d668d6f96a37c1ce90be68238a2dc84455cc4
Rajesh Rohan <rajesh@Galaxy-Air.local>
2026-02-21 14:29:30 +0530

    Clean commit without node_modules

--- reduxCore.js
+++ reduxCore.js
@@ -1,101 +1,98 @@
-import {
-	createStore,
-	applyMiddleware,
-	combineReducers
-} from 'redux';
-import logger from 'redux-logger';
-import axios from 'axios';
-import { thunk } from 'redux-thunk';
+import { createStore, applyMiddleware, combineReducers } from "redux";
+import logger from "redux-logger";
+import axios from "axios";
+import { thunk } from "redux-thunk";
 
-// Constants 
-const Increment = 'account/increment';
-const Decrement = 'account/decrement';
-const IncrementByAmount = 'account/incrementByAmount';
-const IncrementBonus = 'bonus/increment';
+// Constants
+const Increment = "account/increment";
+const Decrement = "account/decrement";
+const IncrementByAmount = "account/incrementByAmount";
+const IncrementBonus = "bonus/increment";
 
-const GetUserPending = 'account/getUser/pending';
-const GetUserFullfilled = 'account/getUser/fullfilled';
-const GetUserRejected = 'account/getUser/rejected';
+const GetUserPending = "account/getUser/pending";
+const GetUserFullfilled = "account/getUser/fullfilled";
+const GetUserRejected = "account/getUser/rejected";
 
 const store = createStore(
-	combineReducers({
-		account: accountReducer,
-		bonus: bonusReducer
-	}),
-	applyMiddleware(logger.default, thunk));
+  combineReducers({
+    account: accountReducer,
+    bonus: bonusReducer,
+  }),
+  applyMiddleware(logger.default, thunk),
+);
 
 const history = [];
 
 function accountReducer(state = { amount: 0 }, action) {
-	switch (action.type) {
-		case GetUserPending:
-			return { ...state, pending : true };
-		case GetUserFullfilled:
-			return { amount: action.payload, pending : false };
-		case GetUserRejected:
-			return { ...state, error: action.error , pending : false};
-		case Increment:
-			return { amount: state.amount + 1 };
-		case Decrement:
-			return { amount: state.amount - 1 };
-		case IncrementByAmount:
-			return { amount: state.amount + action.payload };
-		default:
-			return state;
-	}
+  switch (action.type) {
+    case GetUserPending:
+      return { ...state, pending: true };
+    case GetUserFullfilled:
+      return { amount: action.payload, pending: false };
+    case GetUserRejected:
+      return { ...state, error: action.error, pending: false };
+    case Increment:
+      return { amount: state.amount + 1 };
+    case Decrement:
+      return { amount: state.amount - 1 };
+    case IncrementByAmount:
+      return { amount: state.amount + action.payload };
+    default:
+      return state;
+  }
 }
 
-function bonusReducer(state = { "points": 0 }, action) {
-	switch (action.type) {
-		case IncrementByAmount:
-			return { points: state.points + 1 };
-		case IncrementBonus:
-			return { points: state.points + Math.floor(action.payload / 100) };
-		default:
-			return state;
-	}
+function bonusReducer(state = { points: 0 }, action) {
+  switch (action.type) {
+    case IncrementByAmount:
+      return { points: state.points + 1 };
+    case IncrementBonus:
+      return { points: state.points + Math.floor(action.payload / 100) };
+    default:
+      return state;
+  }
 }
 
 store.subscribe(() => {
-	history.push(store.getState());
-	console.log(history);
+  history.push(store.getState());
+  console.log(history);
 });
 
 // Action creators
 function getUserAccount(id) {
-	return async function (dispatch, getState) {
-		try {
-			dispatch(getUserAccountPending(true));
-			const { data } = await (axios.get(`http://localhost:3000/account/${id}`));
-			dispatch(getUserAccountFullfilled(data.amount));
-		} catch (error) {
-			dispatch(getUserAccountRejected(error.message));
-		}
-	}
+  return async function (dispatch, getState) {
+    try {
+      dispatch(getUserAccountPending(true));
+      const { data } = await axios.get(`http://localhost:3000/account/${id}`);
+      dispatch(getUserAccountFullfilled(data.amount));
+    } catch (error) {
+      dispatch(getUserAccountRejected(error.message));
+    }
+  };
 }
 function getUserAccountPending() {
-	return { type: GetUserPending, pending : true };
+  return { type: GetUserPending, pending: true };
 }
 function getUserAccountFullfilled(value) {
-	return { type: GetUserFullfilled, payload: value };
+  return { type: GetUserFullfilled, payload: value };
 }
 function getUserAccountRejected(value) {
-	return { type: GetUserRejected, error: value };
+  return { type: GetUserRejected, error: value };
 }
 function increment() {
-	return { type: Increment }
+  return { type: Increment };
 }
 function decrement() {
-	return { type: Decrement }
+  return { type: Decrement };
 }
 function incrementByAmount(value) {
-	return { type: IncrementByAmount, payload: value }
+  return { type: IncrementByAmount, payload: value };
 }
 function incrementBonus(value) {
-	return { type: IncrementBonus, payload: value }
+  return { type: IncrementBonus, payload: value };
 }
 
 setTimeout(() => {
-	store.dispatch(getUserAccount(1));
-	// store.dispatch(incrementBonus(200));
+   store.dispatch(getUserAccount(1));
+  // store.dispatch(incrementBonus(200));
 }, 2000);
